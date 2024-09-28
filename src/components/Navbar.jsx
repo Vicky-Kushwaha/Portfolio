a03d088409e5b476040b3a28735119d61629fda1
Vicky-Kushwaha <muzvickykerma12345@gmail.com>
2024-09-28 18:47:49 +0530

    minor change in style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,50 @@
-import "./css/navbar.css";  
-
+import "./css/navbar.css";
 
 const Navbar = () => {
-	return(   
-       <>
-<nav className="navbar navbar-expand-lg " id="navbar">
-  <div className="container">
-    <div className="nav_heading">Vicky Kushwaha</div>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#home">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#about">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#projects">Project</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#education">Education</a>
-        </li>                
-      </ul> 
-    
-    </div>
-  </div>
-</nav>
-
-     </>
-       
-		);
-}  
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg " id="navbar">
+        <div className="container">
+          <div className="nav_heading">Vicky Kumar</div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">
+                  Project
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#education">
+                  Education
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,40 +8,47 @@ const Projects = () => {
       </h3>
       <div className="projects_container">
         <div className="project">
-            <img src="./images/ecommerce.png" alt="poster" className="poster"/>
           <div>
-            <h4>Ecommerce</h4>
+            <img src="./images/ecommerce.png" alt="poster" className="poster" />
           </div>
-          <div>
-            <p>
-            This feature-rich ecommerce website, developed from scratch using ReactJS, Redux, NodeJS, ExpressJS, MongoDB, and Material UI, I created seamless user experiences for product management, order processing, and user authentication and many more features.
-            </p>
-          </div>
-          <div className="view">
-            <a
-              href="https://ecommerce-pearl-eta.vercel.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="./images/visit.png" alt="visit" width="45"  />
-            </a>
-            <a
-              href="https://github.com/Vicky-Kushwaha/Ecommerce"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src="images/code.png" alt="code" width="30" />
-            </a>
+          <div className="projectDetails">
+            <div>
+              <h4>Ecommerce</h4>
+            </div>
+            <div>
+              <p>
+                This feature-rich ecommerce website, developed from scratch
+                using ReactJS, Redux, NodeJS, ExpressJS, MongoDB, and Material
+                UI, I created seamless user experiences for product management,
+                order processing, and user authentication and many more
+                features.
+              </p>
+            </div>
+            <div className="view">
+              <a
+                href="https://ecommerce-pearl-eta.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./images/visit.png" alt="visit" width="45" />
+              </a>
+              <a
+                href="https://github.com/Vicky-Kushwaha/Ecommerce"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="images/code.png" alt="code" width="30" />
+              </a>
+            </div>
           </div>
         </div>
 
         <div className="project">
-        <img src="./images/school.png" alt="poster" className="poster"/>
+          <img src="./images/school.png" alt="poster" className="poster" />
           <div>
             <h4>School Management System</h4>
           </div>
           <div>
-            
             <p>
               This MERN-based School Management System offers user-specific
               features, allowing seamless login experiences for administrators,
@@ -55,7 +62,7 @@ const Projects = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="./images/visit.png" alt="visit" width="45"  />
+              <img src="./images/visit.png" alt="visit" width="45" />
             </a>
             <a
               href="https://github.com/Vicky-Kushwaha?tab=repositories"
@@ -68,7 +75,7 @@ const Projects = () => {
         </div>
 
         <div className="project">
-        <img src="./images/weather.png" alt="poster" className="poster"/>
+          <img src="./images/weather.png" alt="poster" className="poster" />
           <div>
             <h4>Weather App</h4>
           </div>
